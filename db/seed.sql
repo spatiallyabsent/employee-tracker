e239c313a73f7687a74d144d1d79b327764bd75e
SpatiallyAbsent <joshuapdow@gmail.com>
2024-01-22 16:36:03 -0700

    added employee role update functions and changed seedsql

--- db/seed.sql
+++ db/seed.sql
@@ -6,21 +6,21 @@ VALUES ('Sales'),
        ('Legal');
 
 INSERT INTO company_role (role_title, salary, department_id)
-VALUES ('Salesperson', 80000, 1),
-       ('Lead Engineer', 120000, 2),
-       ('Software Engineer', 100000, 2),
-       ('Account Manager', 160000, 3),
-       ('Accountant', 125000, 3),
-       ('Legal Team Lead', 250000, 4),
-       ('Lawyer', 190000, 4);
+VALUES ('Salesperson', 80000.00, 1),
+       ('Lead Engineer', 120000.00, 2),
+       ('Software Engineer', 100000.00, 2),
+       ('Account Manager', 160000.00, 3),
+       ('Accountant', 125000.00, 3),
+       ('Legal Team Lead', 250000.00, 4),
+       ('Lawyer', 190000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Mark', 'Plier', 6, NULL),
-       ('Mad', 'Vladd', 7, 1),
-       ('Eva', 'Willson', 5, 5),
-       ('Kiwi', 'Nirvana', 5, 5),
-       ('James', 'Steinheuser', 4, NULL),
-       ('Billy', 'Billbert', 3, 7),
-       ('Jay', 'Proguy', 2, NULL),
-       ('Willy', 'Wonka', 1, 7),
-       ('Herbert', 'Hershey', 1, 7);
+VALUES ('Mark', 'Plier', 6, 1),
+       ('Mad', 'Vladd', 7, NULL),
+       ('Eva', 'Willson', 5, NULL),
+       ('Kiwi', 'Nirvana', 5, NULL),
+       ('James', 'Steinheuser', 4, 5),
+       ('Billy', 'Billbert', 3, NULL),
+       ('Jay', 'Proguy', 2, 7),
+       ('Willy', 'Wonka', 1, NULL),
+       ('Herbert', 'Hershey', 1, NULL);
--- index.js
+++ index.js
@@ -22,54 +22,54 @@ db.connect((err) => {
 
 function startEmployeeTracker() {
     inquirer
-      .prompt({
-        type: 'list',
-        name: 'menuChoice',
-        message: 'Pick an option',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
-        ],
-      })
-      .then((answers) => {
-        switch (answers.menuChoice) {
-            case 'View all departments':
-                getAllDepartments();
-                break;
-            case 'View all roles':
-                getAllRoles();
-                break;
-            case 'View all employees':
-                getAllEmployees();
-                break;
-            case 'Add a department':
-                addDepartment();
-                break;
-            case 'Add a role':
-                addNewRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                //add function after writing it
-                break;
-            case 'EXIT':
-              db.end();
-              console.log('Ending Program');
-              break;
-        }
-      });
+        .prompt({
+            type: 'list',
+            name: 'menuChoice',
+            message: 'Pick an option',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ],
+        })
+        .then((answers) => {
+            switch (answers.menuChoice) {
+                case 'View all departments':
+                    getAllDepartments();
+                    break;
+                case 'View all roles':
+                    getAllRoles();
+                    break;
+                case 'View all employees':
+                    getAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addNewRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'EXIT':
+                    db.end();
+                    console.log('Ending Program');
+                    break;
+            }
+        });
 }
 
 //to handle Database Queries to get all departments
 function getAllDepartments() {
-    db.query('SELECT * FROM department', (err, res) =>{
+    db.query('SELECT * FROM department', (err, res) => {
         if (err) throw err;
         console.table(res);
         startEmployeeTracker();
@@ -77,7 +77,7 @@ function getAllDepartments() {
 }
 //queries for view all roles
 function getAllRoles() {
-    db.query('SELECT * FROM company_role', (err, res) =>{
+    db.query('SELECT * FROM company_role', (err, res) => {
         if (err) throw err;
         console.table(res);
         startEmployeeTracker();
@@ -85,7 +85,7 @@ function getAllRoles() {
 }
 //queries for view all employees
 function getAllEmployees() {
-    db.query('SELECT * FROM employee', (err, res) =>{
+    db.query('SELECT first_name, last_name, role_id, manager_id FROM employee', (err, res) => {
         if (err) throw err;
         console.table(res);
         startEmployeeTracker();
@@ -95,98 +95,115 @@ function getAllEmployees() {
 //add code to add a department here
 function addDepartment() {
     inquirer
-      .prompt({
-        type: 'input',
-        name: 'department_name',
-        message: 'Enter the name of the new department:',
-      })
-      .then((answers) => {
-        db.query(
-            'INSERT INTO department (name) VALUES (?)',
-            [answers.department_name],
-            (err, res) => {
-                if (err) throw err;
-                console.log('Department successfully added');
-                startEmployeeTracker();
-            }
-        );
-      });
+        .prompt({
+            type: 'input',
+            name: 'department_name',
+            message: 'Enter the name of the new department:',
+        })
+        .then((answers) => {
+            db.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [answers.department_name],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Department successfully added');
+                    startEmployeeTracker();
+                }
+            );
+        });
 }
 //add code to add a role here
 function addNewRole() {
     inquirer
-      .prompt([
-        {
-        type: 'input',
-        name: 'role_title',
-        message: 'Enter the name of the new role:',
-        },
-        {
-        type: 'input',
-        name: 'salary',
-        message: 'Enter the salary for the new role:',
-        validate: (input) => {
-        const isValidAmount = !isNaN(input) && input > 0;
-        return isValidAmount || 'Please use a valid poitive number for salary.';
-          },
-        },
-        {
-            type: 'list',
-            name: 'department_id',
-            message: 'Select the department for the new role',
-            choices: getDepartmentChoices(),
-        },
-      ])
-      .then((answers) => {
-        db.query(
-            'INSERT INTO company_role (role_title, salary, department_id) VALUES (?, ?, ?)',
-            [answers.role_title, answers.salary, answers.department_id],
-            (err, res) => {
-                if (err) throw err;
-                console.log('Role successfully added');
-                startEmployeeTracker();
+        .prompt([
+            {
+                type: 'input',
+                name: 'role_title',
+                message: 'Enter the name of the new role:',
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Enter the salary for the new role:',
+                validate: (input) => {
+                    const isValidAmount = !isNaN(input) && input > 0;
+                    return isValidAmount || 'Please use a valid poitive number for salary.';
+                },
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: 'Select the department for the new role',
+                choices: () => getDepartmentChoices(),
+            },
+        ])
+        .then((answers) => {
+            db.query(
+                'INSERT INTO company_role (role_title, salary, department_id) VALUES (?, ?, ?)',
+                [answers.role_title, answers.salary, answers.department_id],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Role successfully added');
+                    startEmployeeTracker();
+                }
+            );
+        });
+}
+//allows us to choose a department for adding a new role
+function getDepartmentChoices() {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT id, name FROM department', (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                const choices = results.map((department) => ({
+                    name: department.name,
+                    value: department.id,
+                }));
+                console.log('Choicces:', choices);
+                resolve(choices);
             }
-        );
-      });
-    }
+        });
+    });
+}
 //How to add an employee
 function addEmployee() {
     inquirer
-      .prompt([
-        {
-            type: 'input',
-            name: 'first_name',
-            message: 'Type employees first name:',
-        },
-        {
-            type: 'input',
-            name: 'last_name',
-            message: 'Type employees last name:',
-        },
-        {
-            type: 'list',
-            name: 'role_id',
-            message: 'Pick your employees role:',
-            choices: getRoleChoices(),
-        },
-        {
-            type: 'list',
-            name: 'manager_id',
-            message: 'Pick your employees manager:',
-            choices: getManagerChoices(),
-        },
-      ])
-      .then((answers) =>{
-        db.query(
-            'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
-            [answers.first_name, answers.last_name, answers.role_id, answers.manager_id],
-            (err, res) => {
-                if (err) throw err;
-                console.log('Employee successfully added');
-                startEmployeeTracker();
-            }
-        );
-      });
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Type employees first name:',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Type employees last name:',
+            },
+            {
+                type: 'list',
+                name: 'role_id',
+                message: 'Pick your employees role:',
+                choices: () => getRoleChoices(),
+            },
+            {
+                type: 'list',
+                name: 'manager_id',
+                message: 'Pick your employees manager:',
+                choices: () => getManagerChoices(),
+            },
+        ])
+        .then((answers) => {
+            db.query(
+                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+                [answers.first_name, answers.last_name, answers.role_id, answers.manager_id],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Employee successfully added');
+                    startEmployeeTracker();
+                }
+            );
+        });
 }
 //lets user assign a role
 function getRoleChoices() {
@@ -195,7 +212,7 @@ function getRoleChoices() {
             if (err) {
                 reject(err);
             } else {
-                const choices =results.map((role) => ({
+                const choices = results.map((role) => ({
                     name: role.role_title,
                     value: role.id,
                 }));
@@ -211,7 +228,7 @@ function getManagerChoices() {
             if (err) {
                 reject(err);
             } else {
-                const choices =results.map((manager) => ({
+                const choices = results.map((manager) => ({
                     name: `${manager.first_name} ${manager.last_name}`,
                     value: manager.id,
                 }));
@@ -221,8 +238,54 @@ function getManagerChoices() {
     });
 }
 
+//updating the employee role--get employee choices
+function getEmployeeChoices() {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT id, first_name, last_name FROM employee', (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                const choices = results.map((employee) => ({
+                    name: `${employee.first_name} ${employee.last_name}`,
+                    value: employee.id,
+                }));
+                resolve(choices);
+            }
+        });
+    });
+}
+//updating the employee role--updating the role after getting the choices
+function updateEmployeeRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employee_id',
+                message: 'Select an employee to update their role',
+                choices: () => getEmployeeChoices(),
+            },
+            {
+                type: 'list',
+                name: 'new_role_id',
+                message: 'Select the new role for the emmployee',
+                choices: () => getRoleChoices(),
+            },
+        ])
+        .then((answers) => {
+            db.query(
+                'UPDATE employee SET role_id = ? WHERE id = ?',
+                [answers.new_role_id, answers.employee_id],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Employee role updated');
+                    startEmployeeTracker();
+                }
+            );
+        });
+}
+
 //used to ensure the closing from the database, meant to be last line of code.
-process.on('SIGINT', () =>{
+process.on('SIGINT', () => {
     db.end((err) => {
         if (err) {
             console.error('Problems disconnecting', err);
